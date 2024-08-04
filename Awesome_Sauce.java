50a2fe5514ae84bbdd89f61c4b1fd052bf1d538a
Abubakar081013 <Abubakar081013@gmail.com>
2024-08-04 16:31:31 -0500

    Create Awesome_Sauce.java

---
+++ Awesome_Sauce.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Awesome_Sauce {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        for(int cool = 1; cool < 8; cool++){
+            for(int awesome_sauce = 1; awesome_sauce <= 8 - cool; awesome_sauce++){
+            System.out.print(awesome_sauce);
+            }
+            System.out.println();
+        }
+    }
+}
